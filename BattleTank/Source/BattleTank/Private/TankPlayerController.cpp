7be6f9bf0f61b7fe66613ea30e5a95a7f8097e09
Jeff Brown <jeffjjbrown@gmail.com>
2020-05-10 10:51:45 -0700

    Updates through tank and tank player controller code generation

---
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -0,0 +1,23 @@
+// Copyright Jeff Brown 2020.
+
+#include "TankPlayerController.h"
+
+ATank *ATankPlayerController::GetControlledTank() const
+{
+  return Cast<ATank>(GetPawn());
+}
+
+void ATankPlayerController::BeginPlay()
+{
+  Super::BeginPlay();
+
+  auto ControlledTank = GetControlledTank();
+  if (!ControlledTank)
+  {
+    UE_LOG(LogTemp, Warning, TEXT("PlayerController not possessing a tank..."));
+  }
+  else
+  {
+    UE_LOG(LogTemp, Warning, TEXT("PlayerController possessing: %s"), *(ControlledTank->GetName()));
+  }
+}
---
+++ BattleTank/Source/BattleTank/Public/TankPlayerController.h
@@ -0,0 +1,24 @@
+// Copyright Jeff Brown 2020.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+
+#include "Tank.h"
+
+#include "TankPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLETANK_API ATankPlayerController : public APlayerController
+{
+  GENERATED_BODY()
+
+public:
+  ATank *GetControlledTank() const;
+
+  void BeginPlay() override;
+};
