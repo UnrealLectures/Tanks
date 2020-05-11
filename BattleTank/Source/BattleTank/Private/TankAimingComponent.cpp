2d649a962922842908b5a5a2a46b6fb22e29eeb2
Jeff Brown <jeffjjbrown@gmail.com>
2020-05-10 17:58:27 -0700

    Updated aiming component to have a launch speed

--- BattleTank/Source/BattleTank/Private/TankAimingComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankAimingComponent.cpp
@@ -28,8 +28,12 @@ void UTankAimingComponent::TickComponent(float DeltaTime, ELevelTick TickType, F
   // ...
 }
 
-void UTankAimingComponent::AimAt(FVector HitLocation)
+void UTankAimingComponent::AimAt(FVector HitLocation, float LaunchSpeed)
 {
-  auto OurTankName = GetOwner()->GetName();
-  UE_LOG(LogTemp, Warning, TEXT("%s is aiming at: %s"), *OurTankName, *HitLocation.ToString());
+  UE_LOG(LogTemp, Warning, TEXT("Firing at %f"), LaunchSpeed);
+}
+
+void UTankAimingComponent::SetBarrelReference(UStaticMeshComponent *BarrelToSet)
+{
+  Barrel = BarrelToSet;
 }
--- BattleTank/Source/BattleTank/Public/Tank.h
+++ BattleTank/Source/BattleTank/Public/Tank.h
@@ -18,6 +18,9 @@ public:
   // Sets default values for this pawn's properties
   ATank();
 
+  UFUNCTION(BlueprintCallable, Category = Setup)
+  void SetBarrelReference(UStaticMeshComponent *BarrelToSet);
+
   // Called every frame
   virtual void Tick(float DeltaTime) override;
 
@@ -31,4 +34,8 @@ protected:
   virtual void BeginPlay() override;
 
   UTankAimingComponent *TankAimingComponent = nullptr;
+
+private:
+  UPROPERTY(EditAnywhere, Category = Firing)
+  float LaunchSpeed = 100000;
 };
